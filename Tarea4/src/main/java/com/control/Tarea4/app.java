359aa23e18c7141ca01cb27b941e7a51b63213e4
katherineggs <katherinegarcia@ufm.edu>
2020-02-01 00:27:36 -0600

    Functional Tarea 3

--- Tarea4/src/main/java/com/control/Tarea4/app.java
+++ Tarea4/src/main/java/com/control/Tarea4/app.java
@@ -1,4 +1,32 @@
 package com.control.Tarea4;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class app {
+    int creditos[];
+    int debitos[];
+    int size = 10;
+
+    public void control(){
+        creditos = new int[size];
+        debitos = new int[size];
+    }
+
+    public int totalDebs(){
+        int total = 0;
+        for(int i = 0; i < size; i++){
+            total = total + debitos[i];
+        }
+        return total;
+    }
+    public int totalCreds(){
+        int total = 0;
+        for(int i = 0; i < size; i++){
+            total = total + creditos[i];
+        }
+        return total;
+    }
+
+
 }
--- tarea3/src/main/java/com/queue/tarea3/app.java
+++ tarea3/src/main/java/com/queue/tarea3/app.java
@@ -5,61 +5,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @Controller
 public class app {
 //    ArrayList<String> queue = new ArrayList<>();
-    String queue[];
     int capacity = 0;
-    int index = -1;
+    int index = 0;
 
-    public void Bank(){
-        queue = new String[15];
+    public static String[] Bank(){
+        String q[] = new String[5];
+        return q;
     }
+    String queue[] = Bank();
 
     @RequestMapping(value = {"/push"})
     @ResponseBody
     public String[] pushQueue(@RequestParam String info){
         queue[index++]=info;
         capacity ++;
-        if(capacity >= 15){
-            String new_queue[] = new String[capacity*2];
-            return  new_queue;
-        }
-//        for(int i = 0; i < 15; i++){
-//            if (queue[i].equals(0)){
-//                queue[i]= info;
-//            }
+//        if(capacity >= 5){
+//            String new_queue[] = new String[capacity*2];
+//            return  new_queue;
 //        }
-//        Tarea3.push(info ,queue);
-        else{
-            return queue;
-        }
+////        for(int i = 0; i < 5; i++){
+////            if (queue[i].equals(0)){
+////                queue[i]= info;
+////            }
+////        }
+////        Tarea3.push(info ,queue);
+//        else{
+//            return queue;
+//        }
+        return queue;
     }
     @RequestMapping(value = {"/pop"})
     @ResponseBody
     public String[] popQueue(){
-        for(int i = 0; i > 15; i++ ){
+        for(int i = 0; i < 5 - 1; i++ ){
             queue[i] = queue[i+1];
+            queue[5 -1] = null;
         }
         return queue;
     }
     @RequestMapping(value = {"/clear"})
     @ResponseBody
     public String[] clearQueue(){
-        for(int i = 0; i > 15; i++ ){
-            queue[i] = "cleared";
+        for(int i = 0; i < 5; i++ ){
+            queue[i] = null;
         }
-//        Tarea3.clear(queue);
         return queue;
     }
     @RequestMapping(value = {"/show"})
     @ResponseBody
     public String[] showQueue(){
-
-//        Tarea3.show(queue);
         return queue;
     }
 
