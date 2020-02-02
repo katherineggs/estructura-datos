1f1ea8dfee26b3d1ab3ab8ec20cc9a5e0275b4d7
katherineggs <katherinegarcia@ufm.edu>
2020-02-02 08:02:00 -0600

    Tarea

--- Tarea4/src/main/java/com/control/Tarea4/app.java
+++ Tarea4/src/main/java/com/control/Tarea4/app.java
@@ -4,29 +4,85 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class app {
-    int creditos[];
-    int debitos[];
-    int size = 10;
-
-    public void control(){
-        creditos = new int[size];
-        debitos = new int[size];
+    public static String[] Debs(){
+        String debitos[] = new String[20];
+        return debitos;
     }
+    public static String[] Creds(){
+        String[] creditos = new String[20];
+        return creditos;
+    }
+    String creditos[] = Creds();
+    String debitos[] = Debs();
 
-    public int totalDebs(){
-        int total = 0;
-        for(int i = 0; i < size; i++){
+    public String totalDebs(){
+        String total = "0"; //DEBE SER INT O ALGO ASI
+        for(int i = 0; i < 20 -1; i++){
             total = total + debitos[i];
         }
         return total;
     }
-    public int totalCreds(){
-        int total = 0;
-        for(int i = 0; i < size; i++){
+    public String totalCreds(){
+        String total = "0"; //DEBE SER INT O ALGO ASI
+        for(int i = 0; i < 20 -1; i++){
             total = total + creditos[i];
         }
         return total;
     }
 
+    public String Saldo(){
+        //total debitos - total creditos
+        return "hola";
+    }
+    public String promDebs(){
+        int cantidad = 0;
+        for(int i = 0; i < 20 - 1; i++){
+            if(debitos[i].isEmpty()){
+                cantidad = cantidad;
+            }
+            else{
+                cantidad ++;
+            }
+        }
+        //total debitos / cantidad en debs
+        return"hola";
+    }
+
+    public String biggerDebs(){
+        String bigger;
+        for(int i = 0; i < 20 - 1; i++){
+//            if(debitos[i] < debitos[i+1]){ //DEBE SER INT O ALGO ASI
+//                debitos[i] = debitos[i+1] //Y ALGO MAS
+//            }
+            bigger = debitos[i]; //La primer casilla
+        }
+        return "ola";//bigger
+    }
+
+    
+//    int size = 10;
+//    public static String[] debis(size){
+//        String debitos[] = new String[size];
+//        return debitos;
+//    }
+//    public void credis(){
+//        String creditos[] = new String[size];
+//    }
+//    String debitos =
+//    public String totalDebs(){
+//        int total = 0;
+//        for(int i = 0; i < size; i++){
+//            total = total + debitos[i];
+//        }
+//        return total;
+//    }
+//    public String totalCreds(){
+//        int total = 0;
+//        for(int i = 0; i < size; i++){
+//            total = total + creditos[i];
+//        }
+//        return total;
+//    }
+//
 
 }
--- tarea3/src/main/java/com/queue/tarea3/app.java
+++ tarea3/src/main/java/com/queue/tarea3/app.java
@@ -14,49 +14,45 @@ public class app {
 //    ArrayList<String> queue = new ArrayList<>();
     int capacity = 0;
     int index = 0;
-
-    public static String[] Bank(){
-        String q[] = new String[5];
+    int size = 100;
+    public static String[] Bank(int size){
+        String q[] = new String[size];
         return q;
     }
-    String queue[] = Bank();
-
+    String queue[] = Bank(size);
     @RequestMapping(value = {"/push"})
     @ResponseBody
-    public String[] pushQueue(@RequestParam String info){
-        queue[index++]=info;
-        capacity ++;
-//        if(capacity >= 5){
-//            String new_queue[] = new String[capacity*2];
-//            return  new_queue;
+    public String pushQueue(@RequestParam String info){
+//        capacity ++;
+//        if(capacity >= size){
+
+//        String temp_queue[] = queue;
+//        size = size +1;
+//        String queue[] = Bank(size);
+//
+//        for(int i = 0; i <= size -2; i++){
+//            queue[i] = "hola";
 //        }
-////        for(int i = 0; i < 5; i++){
-////            if (queue[i].equals(0)){
-////                queue[i]= info;
-////            }
-////        }
-////        Tarea3.push(info ,queue);
-//        else{
-//            return queue;
-//        }
-        return queue;
+//        index = index +1;
+        queue[index++] = info;
+        return "Se ha añadido a la cola!";
     }
     @RequestMapping(value = {"/pop"})
     @ResponseBody
-    public String[] popQueue(){
-        for(int i = 0; i < 5 - 1; i++ ){
+    public String popQueue(){
+        for(int i = 0; i < size -1; i++ ){
             queue[i] = queue[i+1];
-            queue[5 -1] = null;
+            queue[size -1] = null;
         }
-        return queue;
+        return "Adios!";
     }
     @RequestMapping(value = {"/clear"})
     @ResponseBody
-    public String[] clearQueue(){
-        for(int i = 0; i < 5; i++ ){
+    public String clearQueue(){
+        for(int i = 0; i < size -1; i++ ){
             queue[i] = null;
         }
-        return queue;
+        return "Banco libre!";
     }
     @RequestMapping(value = {"/show"})
     @ResponseBody
