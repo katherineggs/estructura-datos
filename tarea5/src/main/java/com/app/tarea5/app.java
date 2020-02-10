2b220eaa31d9d90cb69f83f8b9784398465720a0
katherineggs <katherinegarcia@ufm.edu>
2020-02-09 21:15:41 -0600

    Tarea 5

--- tarea5/src/main/java/com/app/tarea5/app.java
+++ tarea5/src/main/java/com/app/tarea5/app.java
@@ -9,22 +9,20 @@ import java.util.Arrays;
 
 @Controller
 public class app {
-    int cantidad[] = new int[30];
-    char letra[] = new char[30];
-    String top[] = new String[30];
-    int indexL = 0;
-    int indexT = 0;
-    char l;
-    String fin = "";
+    static int cantidad[] = new int[30];
+    static String top[] = new String[30];
+    static int indexT = 0;
+    static char l;
+    static String fin = "";
+    static String result = "";
     @RequestMapping(value = {"/text"})
     @ResponseBody
-    public String textCount(@RequestParam String text){
+    public static String textCount(@RequestParam String text){
         text = text.toLowerCase();
         for(int i = 0; i < text.length() + 1; i++){
             text = text.replaceAll("\\s+","");
             String tp = "";
             int cant = 0;
-            letra[indexL] = text.charAt(0);
             l = text.charAt(0);
             for(int j = 0; j < text.length(); j++) {
                 if (l == text.charAt(j)) {
@@ -34,29 +32,23 @@ public class app {
             }
             text = text.replace(l, ' ');
             cantidad[indexT] = cant;
-            top[indexT] = tp;
+            fin = fin +" "+l+" * "+Integer.toString(cant)+"  "+tp+"\n";
+            top[indexT]= fin;
             indexT++;
-            indexL++;
         }
         for(int mayor = 0; mayor < cantidad.length-1; mayor ++){
             if(cantidad[mayor] < cantidad[mayor + 1] ){
                 int var = cantidad[mayor];
                 cantidad[mayor] = cantidad[mayor+1];
                 cantidad[mayor+1] = var; //numeros
-                char v = letra[mayor];
-                letra[mayor] = letra[mayor+1];
-                letra[mayor+1] = v; //letras
                 String ast = top[mayor];
                 top[mayor] = top[mayor+1];
                 top[mayor+1] = ast; //asteriscos
             }
         }
-        for(int num = 0; num < 10; num ++){
-            fin = fin+" "+ Integer.toString(num+1)+". "+letra[num]+" * "+Integer.toString(cantidad[num])+" "+top[num]+"\n";
+        for(int i =0; i <10; i++){
+            result = result + top[i];
         }
-//        System.out.println(Arrays.toString(cantidad));
-//        System.out.println(Arrays.toString(letra));
-//        System.out.println(fin);
-        return fin;
+        return result;
     }
 }
--- tarea5/src/test/java/com/app/tarea5/appTest.java
+++ tarea5/src/test/java/com/app/tarea5/appTest.java
@@ -8,11 +8,11 @@ class appTest {
 
     @Test
     void textCount() {
-        assertEquals("1. o * 25  *************************\n 2. r * 24  ************************\n " +
+        assertEquals(" 1. o * 25  *************************\n 2. r * 24  ************************\n " +
                 "3. e * 59  ***********************************************************\n " +
                 "4. l * 22  **********************\n 5. i * 38  **************************************\n " +
                 "6. m * 19  *******************\n 7. s * 39  ***************************************\n " +
                 "8. p * 19  *******************\n 9. u * 17  *****************\n " +
-                "10. d * 16  ****************\n", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+                "10. d * 16  ****************\n", app.textCount("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
     }
 }
