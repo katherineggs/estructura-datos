c952f04fc144babaf98a4b114132a50905e3f886
katherineggs <katherinegarcia@ufm.edu>
2020-05-03 13:18:02 -0600

    Binary Search

--- Search&Sort/Tarea Mayo 3/BinarySearch.py
+++ Search&Sort/Tarea Mayo 3/BinarySearch.py
@@ -1,22 +1,28 @@
-# No funciona si el elemento no esta el la lista
+
 def BinarySearch(sortedArray,num,lenghtt,element,steps):
-    half = num + (lenghtt - 1) // 2
+
+    add = num + lenghtt
+    half = add // 2
+
     if(element in sortedArray):
+
         if(sortedArray[half] == element):
-            return "Found in "+ str(steps) + " rounds"
+            return "Found "+ str(element) +" in "+ str(steps) + " rounds"
 
         elif(element < sortedArray[half]):
             steps += 1
-            return BinarySearch(sortedArray,num,half-1,element,steps)
+            lenghtt = half - 1
+            return BinarySearch(sortedArray,num,lenghtt,element,steps)
 
-        elif(element >= sortedArray[half]):
+        elif(element > sortedArray[half]):
             steps += 1
-            return BinarySearch(sortedArray,half+1,lenghtt,element,steps)
+            num = half +1
+            return BinarySearch(sortedArray,num,lenghtt,element,steps)
 
     else:
         return "NotFound"
 
 sortedArray = [0,2,4,5,7,9]
-element = 7
+element = int(input("Element to find: "))
 lenght = len(sortedArray)
 print(BinarySearch(sortedArray,0,lenght-1,element,1))
